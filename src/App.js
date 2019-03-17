0b65d5439ba33e57e7cdea3c21be3e2e7c803bb8
Andy Tan <andytan0727@gmail.com>
2019-03-17 22:48:17 +0800

    feat: style youtube player page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MainPage from "./components/pages/MainPage";
 import YTPlayerPage from "./components/pages/YTPlayerPage";
 
+import './App.scss';
+
 class App extends Component {
   render() {
     return (
--- src/components/pages/YTPlayerPage/index.js
+++ src/components/pages/YTPlayerPage/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import YoutubePlayerIFrame from "../../YoutubePlayerIFrame";
 
-const YTPlayerPage = () => <YoutubePlayerIFrame />;
+import styles from "./styles.module.scss";
+
+const YTPlayerPage = () => {
+  return (
+    <React.Fragment>
+      <div className={styles.ytPlayerDiv}>
+        <h1>Youtube</h1>
+        <YoutubePlayerIFrame />
+        <div className={styles.ctrlBtnGroup}>
+          <button>Loop</button>
+          <button>Random</button>
+          <button>Shuffle</button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default YTPlayerPage;
