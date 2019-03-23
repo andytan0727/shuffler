8f299605ce91a7a147b7bc3f476604acb579ced6
Andy Tan <andytan0727@gmail.com>
2019-03-24 00:52:21 +0800

    style: update footer

--- src/pages/PgFooter/PgFooter.js
+++ src/pages/PgFooter/PgFooter.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styles from "./styles.module.scss";
 
 const PgFooter = () => (
   <footer className={styles.footer}>
-    <div>
-      <section>
-        <ul>
-          <li>Home</li>
-          <li>Player</li>
-          <li>About</li>
-        </ul>
-      </section>
+    <div className={styles.navPanel}>
+      <p>YT Randomizer</p>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/playlistInput">Playlist</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </div>
     <div className={styles.copyright}>&copy; 2019 Andy Tan</div>
   </footer>
