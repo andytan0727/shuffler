47d5cf52fa83bdcc65de61918ff60c44e08c5cc0
Andy Tan <andytan0727@gmail.com>
2019-03-23 11:50:50 +0800

    feat: add song listing in playlistInput page

--- src/components/Chips/SongChips/SongChips.js
+++ src/components/Chips/SongChips/SongChips.js
@@ -1,22 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 import styles from "./styles.module.scss";
 
+const muiStyles = theme => ({
+  list: {
+    width: "100%",
+    maxWidth: "60vw",
+    maxHeight: "65vh",
+    backgroundColor: theme.palette.background.paper,
+    position: "absolute",
+    left: "50%",
+    transform: "translateX(-50%)",
+    overflow: "auto"
+  }
+});
+
 const SongChips = props => {
+  const {
+    ytplaylist: { playlists },
+    classes
+  } = props;
+
   return (
     <div className={styles.songChipsDiv}>
-      <p>testing</p>
-      <p>chips</p>
+      {playlists.length !== 0 && (
+        <List className={classes.list}>
+          {playlists.map(playlist =>
+            playlist.items.map(item => (
+              <ListItem key={item.id}>
+                <Checkbox checked={false} tabIndex={-1} disableRipple />
+                <ListItemText primary={item.snippet.title} />
+                <ListItemSecondaryAction>
+                  <IconButton aria-label="Delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))
+          )}
+        </List>
+      )}
     </div>
   );
 };
 
-const mapStateToProps = ({ ytapi }) => ({
-  ytapi
+SongChips.propTypes = {
+  playlists: PropTypes.array,
+  classes: PropTypes.object.isRequired
+};
+
+const StyledSongChips = withStyles(muiStyles)(SongChips);
+
+const mapStateToProps = ({ ytplaylist }) => ({
+  ytplaylist
 });
 
 export default connect(
   mapStateToProps,
   {}
-)(SongChips);
+)(StyledSongChips);
--- src/components/pages/PlaylistInputPage/PlaylistInputPage.js
+++ src/components/pages/PlaylistInputPage/PlaylistInputPage.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
 import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 import { withStyles } from "@material-ui/core/styles";
 import PlaylistAppBar from "../../BarComponents/PlaylistAppBar";
+import TopBar from "../../BarComponents/TopBar";
 import PlaylistInput from "../../InputComponents/PlaylistInput";
 import SongChips from "../../Chips/SongChips";
 
@@ -9,46 +15,78 @@ import styles from "./styles.module.scss";
 
 const muiStyles = theme => ({
   divider: {
-    width: "500px"
+    width: "100%",
+    marginLeft: 0,
+    marginRight: 0
   }
 });
 
+const CtrlBtnGroup = ({ handleSwipeDivIdxChange }) => (
+  <React.Fragment>
+    {/* <Button variant="contained" color="secondary" aria-label="play">
+      Play
+    </Button> */}
+    {/* <Button variant="contained" color="secondary" aria-label="shuffle">
+      Shuffle
+    </Button> */}
+    <Fab variant="extended" color="secondary" aria-label="play">
+      Play
+    </Fab>
+    <Fab variant="extended" color="secondary" aria-label="shuffle">
+      Shuffle
+    </Fab>
+
+    <Fab
+      variant="extended"
+      // color="secondary"
+      size="small"
+      onClick={handleSwipeDivIdxChange.bind(this, 0)}
+      aria-label="add-more"
+    >
+      <AddIcon />
+      Add more
+    </Fab>
+  </React.Fragment>
+);
+
 const PlaylistInputPage = props => {
   const { classes } = props;
+  const [swipeDivIdx, setSwipeDivIdx] = useState(0);
+
+  const handleSwipeDivIdxChange = value => {
+    setSwipeDivIdx(value);
+  };
+
   return (
     <React.Fragment>
       <PlaylistAppBar />
+      {/* <TopBar /> */}
       <div className={styles.playlistInPgDiv}>
         <div className={styles.inputDiv}>
-          <PlaylistInput />
+          <SwipeableViews
+            index={swipeDivIdx}
+            onChangeIndex={handleSwipeDivIdxChange}
+            className={styles.swipeableDiv}
+          >
+            <PlaylistInput handleSwipeDivIdxChange={handleSwipeDivIdxChange} />
+            <div className={styles.ctrlButtonsDiv}>
+              <CtrlBtnGroup handleSwipeDivIdxChange={handleSwipeDivIdxChange} />
+            </div>
+          </SwipeableViews>
           <Divider variant="middle" className={classes.divider} />
-          {/* <SongChips /> */}
+          <SongChips />
         </div>
-        <section>
-          <h3>How To:</h3>
-          <p>
-            Officia cillum officia sint qui magna officia. Excepteur minim Lorem
-            enim amet nulla cupidatat velit deserunt culpa velit cillum laborum.
-            Labore cillum veniam sunt dolor magna non Lorem ullamco elit. Nulla
-            culpa adipisicing eu ullamco sit nulla reprehenderit eu aliqua
-            nostrud sint esse dolore. Pariatur ea laboris ea sit non tempor.
-            <br />
-            <br />
-            Reprehenderit sunt non voluptate fugiat et non sit ipsum nostrud
-            nostrud excepteur ex amet nisi. Amet minim nulla deserunt proident
-            occaecat fugiat elit labore consectetur mollit. Ea officia sunt
-            magna veniam quis cupidatat labore.
-            <br />
-            <br />
-            Non laboris officia nulla laborum aliquip dolor. Fugiat laborum
-            pariatur id et eu elit id. Consequat excepteur do nulla id
-            exercitation esse minim sit incididunt minim sit reprehenderit sint
-            cillum.
-          </p>
-        </section>
       </div>
     </React.Fragment>
   );
+};
+
+CtrlBtnGroup.propTypes = {
+  handleSwipeDivIdxChange: PropTypes.func.isRequired
+};
+
+PlaylistInputPage.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(muiStyles)(PlaylistInputPage);
