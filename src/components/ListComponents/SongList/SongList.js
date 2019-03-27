2e2efe2a3cffb2c62547c8c7ae6a0568eb8f5407
Andy Tan <andytan0727@gmail.com>
2019-03-27 16:08:26 +0800

    refactor: SongList component
    - Replace List component by material-ui with react-window
    - Style SongList's scrollbar
    - Add script to run local server in https

--- src/components/ListComponents/SongList/SongList.js
+++ src/components/ListComponents/SongList/SongList.js
@@ -2,19 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemText from "@material-ui/core/ListItemText";
-import Checkbox from "@material-ui/core/Checkbox";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
+
+import { FixedSizeList as List } from "react-window";
 
 import styles from "./styles.module.scss";
 
 const muiStyles = theme => ({
   list: {
-    width: "100%",
     maxWidth: "60vw",
     maxHeight: "65vh",
     backgroundColor: theme.palette.background.paper,
@@ -25,6 +19,18 @@ const muiStyles = theme => ({
   }
 });
 
+class ListItem extends React.PureComponent {
+  render() {
+    const { index, style, data } = this.props;
+
+    return (
+      <div className={styles.listItem} style={style}>
+        {data[index].snippet.title}
+      </div>
+    );
+  }
+}
+
 const SongList = props => {
   const {
     ytplaylist: { listToPlay },
@@ -34,18 +40,16 @@ const SongList = props => {
   return (
     <div className={styles.songListDiv}>
       {listToPlay.length !== 0 ? (
-        <List className={classes.list}>
-          {listToPlay.map(song => (
-            <ListItem key={song.id}>
-              <Checkbox checked={false} tabIndex={-1} disableRipple />
-              <ListItemText primary={song.snippet.title} />
-              <ListItemSecondaryAction>
-                <IconButton aria-label="Delete">
-                  <DeleteIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
+        <List
+          height={350}
+          className={styles.songList}
+          itemCount={listToPlay.length}
+          itemSize={65}
+          itemData={listToPlay}
+          itemKey={(index, data) => data[index].id}
+          width={400}
+        >
+          {ListItem}
         </List>
       ) : (
         <div>
