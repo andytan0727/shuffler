80832b38d3cb8511a600c6a1c7de450682a848e0
Andy Tan <andytan0727@gmail.com>
2019-03-28 21:27:55 +0800

    feat(PlaylistInputPage): support mobile width

--- src/pages/PlaylistInputPage/PlaylistInputPage.js
+++ src/pages/PlaylistInputPage/PlaylistInputPage.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import SwipeableViews from "react-swipeable-views";
 import IconButton from "@material-ui/core/IconButton";
 import HelpIcon from "@material-ui/icons/HelpOutline";
+import Modal from "@material-ui/core/Modal";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import { unstable_useMediaQuery as useMediaQuery } from "@material-ui/core/useMediaQuery";
 import PlaylistInput from "../../components/InputComponents/PlaylistInput";
@@ -16,15 +18,38 @@ import HelpGuide from "./HelpGuide";
 import { shufflePlaylist } from "../../store/ytplaylist/action";
 import styles from "./styles.module.scss";
 
-const muiStyles = () => ({});
+const muiStyles = theme => ({
+  paper: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "90%",
+    height: "80vh",
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: "none",
+    overflow: "auto"
+  }
+});
 
 const PlaylistInputPage = props => {
   const { classes, loadedFromDB, shufflePlaylist } = props;
   const [swipeDivIdx, setSwipeDivIdx] = useState(Number(loadedFromDB));
+  const [modalOpen, setModalOpen] = useState(false);
   const matchesTablet = useMediaQuery("(max-width: 780px)");
 
   const handleSwipeDivIdxChange = value => {
     setSwipeDivIdx(value);
+  };
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
   };
 
   return (
@@ -50,9 +75,25 @@ const PlaylistInputPage = props => {
           </div>
         </div>
         {matchesTablet ? (
-          <IconButton className={styles.helpButton} aria-label="help">
-            <HelpIcon />
-          </IconButton>
+          <React.Fragment>
+            <IconButton
+              className={styles.helpButton}
+              aria-label="help"
+              onClick={handleModalOpen}
+            >
+              <HelpIcon />
+            </IconButton>
+            <Modal
+              aria-labelledby="guide"
+              aria-describedby="how to use guide for user"
+              open={modalOpen}
+              onClose={handleModalClose}
+            >
+              <div className={classes.paper}>
+                <HelpGuide />
+              </div>
+            </Modal>
+          </React.Fragment>
         ) : (
           <HelpGuide />
         )}
