635c9c3e1f7af8342d1c9858f1f40662002999d6
Andy Tan <andytan0727@gmail.com>
2019-03-28 09:10:23 +0800

    feat: remove divider and add how to use guide

--- src/pages/PlaylistInputPage/PlaylistInputPage.js
+++ src/pages/PlaylistInputPage/PlaylistInputPage.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import SwipeableViews from "react-swipeable-views";
 import { Link } from "react-router-dom";
-import Divider from "@material-ui/core/Divider";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import { withStyles } from "@material-ui/core/styles";
@@ -15,13 +14,7 @@ import CombinedPlaylist from "../../components/ListComponents/CombinedPlaylist";
 import { shufflePlaylist } from "../../store/ytplaylist/action";
 import styles from "./styles.module.scss";
 
-const muiStyles = () => ({
-  divider: {
-    width: "100%",
-    marginLeft: 0,
-    marginRight: 0
-  }
-});
+const muiStyles = () => ({});
 
 const CtrlBtnGroup = ({ shufflePlaylist, handleSwipeDivIdxChange }) => (
   <React.Fragment>
@@ -83,13 +76,49 @@ const PlaylistInputPage = props => {
               />
             </div>
           </SwipeableViews>
-          <Divider variant="middle" className={classes.divider} />
           <div className={styles.songListTabsDiv}>
             <SongListTabs
               FirstTabComponent={SavedPlaylist}
               SecondTabComponent={CombinedPlaylist}
             />
           </div>
+        </div>
+        <div className={styles.guideDiv}>
+          <h3>How To Use:</h3>
+          <ul>
+            <li>
+              If you are new user, input YouTube playlist or video url as
+              mentioned and press search. Shuffler will take care the rest for
+              you.
+            </li>
+            <br />
+            <li>
+              After you pressed the search button, Shuffler will fetch and save
+              the YouTube playlist/video from YouTube Data API v3 to your
+              machine for future usage.
+            </li>
+            <br />
+            <li>
+              There is a two-tab panel shows up when fetching process is
+              finished.
+              <br />
+              <br />
+              <ul>
+                <li>
+                  <strong>Saved:</strong> Playlist(s) saved to your machine.
+                </li>
+                <li>
+                  <strong>Playing: </strong> Currently playing playlist, a.k.a
+                  the combined playlist from your saved playlist(s).
+                </li>
+              </ul>
+            </li>
+            <br />
+            <li>
+              You may play, shuffler or add more playlist or video as you wish
+              before you proceed to play your Playing playlist. Enjoy.
+            </li>
+          </ul>
         </div>
       </div>
     </React.Fragment>
