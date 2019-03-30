b9368ed9dff532e58491e7b73efaa86984047496
Andy Tan <andytan0727@gmail.com>
2019-03-30 17:34:18 +0800

    feat: update TopBar to include github link

--- src/components/BarComponents/TopBar/TopBar.js
+++ src/components/BarComponents/TopBar/TopBar.js
@@ -6,8 +6,10 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import { withStyles } from "@material-ui/core/styles";
+import { unstable_useMediaQuery as useMediaQuery } from "@material-ui/core/useMediaQuery";
 
 import { ReactComponent as ShufflerTextLogo } from "../../../assets/shufflerTextLogo.svg";
+import { ReactComponent as GithubLogo } from "../../../assets/githubLogo.svg";
 import styles from "./styles.module.scss";
 
 const muiStyles = theme => ({
@@ -20,6 +22,7 @@ const muiStyles = theme => ({
 const TopBar = props => {
   const { classes } = props;
   const [anchorEl, setAnchorEl] = useState(null);
+  const matchesMobile = useMediaQuery("(max-width: 420px)");
 
   const handleMenuButtonClick = e => {
     setAnchorEl(e.currentTarget);
@@ -37,33 +40,53 @@ const TopBar = props => {
         </Link>
         <Link to="/playlistInput">Playlist</Link>
         <Link to="/about">About</Link>
-        <div>
-          <IconButton
-            className={styles.menuButton}
-            color="inherit"
-            aria-label="Menu"
-            onClick={handleMenuButtonClick}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-          >
-            <MenuItem onClick={handleMenuClose}>
-              <Link className={classes.menuAnchor} to="/playlistInput">
-                Playlist
-              </Link>
-            </MenuItem>
-            <MenuItem onClick={handleMenuClose}>
-              <Link className={classes.menuAnchor} to="/about">
-                About
-              </Link>
-            </MenuItem>
-          </Menu>
-        </div>
+        <a
+          href="https://github.com/andytan0727/yt_random_player"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GithubLogo className={styles.githubLogo} />
+        </a>
+
+        {matchesMobile && (
+          <div>
+            <IconButton
+              className={styles.menuButton}
+              color="inherit"
+              aria-label="Menu"
+              onClick={handleMenuButtonClick}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              <MenuItem onClick={handleMenuClose}>
+                <Link className={classes.menuAnchor} to="/playlistInput">
+                  Playlist
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleMenuClose}>
+                <Link className={classes.menuAnchor} to="/about">
+                  About
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleMenuClose}>
+                <a
+                  className={classes.menuAnchor}
+                  href="https://github.com/andytan0727/yt_random_player"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <GithubLogo className={styles.githubLogo} /> Github
+                </a>
+              </MenuItem>
+            </Menu>
+          </div>
+        )}
       </nav>
     </div>
   );
