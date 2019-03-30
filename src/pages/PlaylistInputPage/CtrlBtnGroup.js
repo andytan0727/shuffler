59684bd830e771dfe962181b3e5684c808b10b7c
Andy Tan <andytan0727@gmail.com>
2019-03-30 13:10:38 +0800

    refactor: using rounded icon button for fabs

--- src/pages/PlaylistInputPage/CtrlBtnGroup.js
+++ src/pages/PlaylistInputPage/CtrlBtnGroup.js
@@ -2,39 +2,48 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Fab from "@material-ui/core/Fab";
+import Tooltip from "@material-ui/core/Tooltip";
 import AddIcon from "@material-ui/icons/Add";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
 
 import styles from "./styles.module.scss";
 
 const CtrlBtnGroup = ({ shufflePlaylist, handleSwipeDivIdxChange }) => (
   <div className={styles.ctrlButtonsDiv}>
-    <Fab
-      variant="extended"
-      size="medium"
-      color="secondary"
-      component={Link}
-      to={"/player"}
-      aria-label="play"
-    >
-      Play
-    </Fab>
-    <Fab
-      variant="extended"
-      size="medium"
-      color="secondary"
-      aria-label="shuffle"
-      onClick={shufflePlaylist}
-    >
-      Shuffle
-    </Fab>
+    <Tooltip title="play">
+      <Fab
+        size="medium"
+        color="primary"
+        component={Link}
+        to={"/player"}
+        aria-label="play"
+      >
+        <PlayArrowIcon />
+      </Fab>
+    </Tooltip>
 
-    <Fab
-      size="small"
-      onClick={handleSwipeDivIdxChange.bind(this, 0)}
-      aria-label="add-more"
-    >
-      <AddIcon />
-    </Fab>
+    <Tooltip title="shuffle">
+      <Fab
+        size="medium"
+        color="primary"
+        aria-label="shuffle"
+        onClick={shufflePlaylist}
+      >
+        <ShuffleIcon />
+      </Fab>
+    </Tooltip>
+
+    <Tooltip title="add more">
+      <Fab
+        size="medium"
+        color="secondary"
+        onClick={handleSwipeDivIdxChange.bind(this, 0)}
+        aria-label="add-more"
+      >
+        <AddIcon />
+      </Fab>
+    </Tooltip>
   </div>
 );
 
