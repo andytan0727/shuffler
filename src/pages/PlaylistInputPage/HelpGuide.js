8e43fc2239d8c3c1f6de9335b06d381d1cecafb9
Andy Tan <andytan0727@gmail.com>
2019-03-30 13:02:23 +0800

    fix: amend help guide

--- src/pages/PlaylistInputPage/HelpGuide.js
+++ src/pages/PlaylistInputPage/HelpGuide.js
@@ -4,21 +4,26 @@ import styles from "./styles.module.scss";
 
 const HelpGuide = () => (
   <div className={styles.guideDiv}>
-    <h3>How To Use:</h3>
+    <h3>
+      <span role="img" aria-label="how-to-use">
+        ❓{" "}
+      </span>
+      How To Use:
+    </h3>
     <ul>
       <li>
-        If you are new user, input YouTube playlist or video url as mentioned
-        and press search. Shuffler will take care the rest for you.
+        If you are new user, input YouTube playlist url as mentioned and press
+        search button.
       </li>
       <br />
       <li>
         After you pressed the search button, Shuffler will fetch and save the
-        YouTube playlist/video from YouTube Data API v3 to your machine for
-        future usage.
+        YouTube playlist by using YouTube Data API v3 to your machine for future
+        usage.
       </li>
       <br />
       <li>
-        There is a two-tab panel shows up when fetching process is finished.
+        There is a two-tab panel showing your playlist collections:
         <br />
         <br />
         <ul>
@@ -26,15 +31,20 @@ const HelpGuide = () => (
             <strong>Saved:</strong> Playlist(s) saved to your machine.
           </li>
           <li>
-            <strong>Playing: </strong> Currently playing playlist, a.k.a the
-            combined playlist from your saved playlist(s).
+            <strong>Playing: </strong> List of videos to be played. (All your
+            initially fetched playlists will be <strong>added</strong> to your
+            playing list)
           </li>
         </ul>
       </li>
       <br />
       <li>
-        You may play, shuffler or add more playlist or video as you wish before
-        you proceed to play your Playing playlist. Enjoy.
+        You may play, shuffler or add one or more playlist as you wish before
+        you proceed to play your Playing list.{" "}
+        <span role="img" aria-label="enjoy">
+          Enjoy 🚀
+        </span>
+        .
       </li>
     </ul>
   </div>
