963f6475171c832108edbbb1e8e4bbcdf5a7b752
Andy Tan <andytan0727@gmail.com>
2019-04-12 19:36:12 +0800

    feat: click to switch panel

--- src/components/TabsComponents/InputTabs/InputTabs.js
+++ src/components/TabsComponents/InputTabs/InputTabs.js
@@ -10,10 +10,37 @@ import PlayingPanel from "../../Panels/PlayingPanel/PlayingPanel";
 import styles from "./styles.module.scss";
 
 const InputTabs = (props) => {
+  const [curPanel, setCurPanel] = useState(0);
   const [checkedButton, setCheckedButton] = useState("radio-videolist");
+  const radios = ["radio-videolist", "radio-video", "radio-playing"];
+  const panels = [
+    {
+      panelIdx: 0,
+      class: styles.videolistPanel,
+      radio: "radio-videolist",
+      Component: VideoListPanel,
+    },
+    {
+      panelIdx: 1,
+      class: styles.videoPanel,
+      radio: "radio-video",
+      Component: VideosPanel,
+    },
+    {
+      panelIdx: 2,
+      class: styles.playingPanel,
+      radio: "radio-playing",
+      Component: PlayingPanel,
+    },
+  ];
 
   const handleChangePanel = (e) => {
     setCheckedButton(e.target.value);
+  };
+
+  const handleClickSwitchPanel = (e) => {
+    const panel = e.currentTarget.getAttribute("data-panel");
+    setCheckedButton(radios[panel]);
   };
 
   return (
@@ -24,30 +51,20 @@ const InputTabs = (props) => {
       />
 
       <div className={styles.playlistPanelsDiv}>
-        <div
-          className={classNames(styles.videolistPanel, {
-            [styles.activePanel]: checkedButton === "radio-videolist",
-            [styles.inactivePanel]: checkedButton !== "radio-videolist",
-          })}
-        >
-          <VideoListPanel />
-        </div>
-        <div
-          className={classNames(styles.videoPanel, {
-            [styles.activePanel]: checkedButton === "radio-video",
-            [styles.inactivePanel]: checkedButton !== "radio-video",
-          })}
-        >
-          <VideosPanel />
-        </div>
-        <div
-          className={classNames(styles.playingPanel, {
-            [styles.activePanel]: checkedButton === "radio-playing",
-            [styles.inactivePanel]: checkedButton !== "radio-playing",
-          })}
-        >
-          <PlayingPanel />
-        </div>
+        {panels.map((panel) => (
+          <div
+            className={classNames(panel.class, {
+              [styles.activePanel]: checkedButton === panel.radio,
+              [styles.inactivePanel]: checkedButton !== panel.radio,
+            })}
+            data-panel={panel.panelIdx}
+            onClick={
+              checkedButton !== panel.radio ? handleClickSwitchPanel : null
+            }
+          >
+            <panel.Component />
+          </div>
+        ))}
       </div>
     </div>
   );
