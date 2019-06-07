3c7a0b621a169144ea52baa81a29260c008fdd0f
Andy Tan <andytan0727@gmail.com>
2019-06-07 12:43:52 +0800

    refactor(preferences): move async logic to thunk

--- src/store/userPreferences/action.js
+++ src/store/userPreferences/action.js
@@ -1,17 +1,28 @@
 import { SET_PREFER_DARK_THEME } from "../../utils/constants/actionConstants";
+import { dbPreferences } from "../../utils/helper/dbHelper";
 
 /**
  * Set user theme preference to Redux store
  * @param {boolean} persist persist to indexedDB
  * @param {boolean} isPreferDarkTheme is user prefer dark mode or not
- * @returns SET_PREFER_DARK_THEME action object for redux store
+ * @returns SET_PREFER_DARK_THEME thunk function for redux store
  */
-const setPreferDarkTheme = ({ persist, isPreferDarkTheme }) => ({
-  type: SET_PREFER_DARK_THEME,
-  payload: {
-    persist,
-    isPreferDarkTheme,
-  },
-});
+const setPreferDarkTheme = ({ persist, isPreferDarkTheme }) => {
+  return (dispatch) => {
+    dispatch({
+      type: SET_PREFER_DARK_THEME,
+      payload: {
+        isPreferDarkTheme,
+      },
+    });
+
+    // persist to indexedDB
+    if (persist) {
+      dbPreferences
+        .setItem("darkTheme", isPreferDarkTheme)
+        .then(() => console.log("successfully saved preferred theme"));
+    }
+  };
+};
 
 export { setPreferDarkTheme };
--- src/store/userPreferences/reducer.js
+++ src/store/userPreferences/reducer.js
@@ -1,5 +1,4 @@
 import { SET_PREFER_DARK_THEME } from "../../utils/constants/actionConstants";
-import { dbPreferences } from "../../utils/helper/dbHelper";
 
 const initialStates = {
   preferDarkTheme: true,
@@ -8,18 +7,9 @@ const initialStates = {
 export const userPreferences = (state = initialStates, action) => {
   switch (action.type) {
     case SET_PREFER_DARK_THEME: {
-      const isPreferDarkTheme = action.payload.isPreferDarkTheme;
-
-      // persist to indexedDB
-      if (action.payload.persist) {
-        dbPreferences
-          .setItem("darkTheme", isPreferDarkTheme)
-          .then(() => console.log("successfully saved preferred theme"));
-      }
-
       return {
         ...state,
-        preferDarkTheme: isPreferDarkTheme,
+        preferDarkTheme: action.payload.isPreferDarkTheme,
       };
     }
 
