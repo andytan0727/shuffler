84e5bc5d5904df1b2880a3bddbee6537ccafc57d
Andy Tan <andytan0727@gmail.com>
2019-06-27 10:51:05 +0800

    test(userPreferences): action + reducer

---
+++ src/store/userPreferences/__tests__/action.js
@@ -0,0 +1,15 @@
+import { SET_PREFER_DARK_THEME } from "../../../utils/constants/actionConstants";
+import { setPreferDarkTheme } from "../action";
+
+describe("userPreferences actions", () => {
+  test("should create SET_PREFER_DARK_THEME action object", () => {
+    const isPreferDarkTheme = true;
+
+    expect(setPreferDarkTheme(isPreferDarkTheme)).toEqual({
+      type: SET_PREFER_DARK_THEME,
+      payload: {
+        isPreferDarkTheme: true,
+      },
+    });
+  });
+});
---
+++ src/store/userPreferences/__tests__/reducer.js
@@ -0,0 +1,24 @@
+import { setPreferDarkTheme } from "../action";
+import { userPreferences as userPreferencesReducer } from "../reducer";
+
+describe("userPreferences reducer", () => {
+  test("should return initial state on default", () => {
+    expect(userPreferencesReducer(undefined, {})).toEqual({
+      preferDarkTheme: true,
+    });
+  });
+
+  test("should handle SET_PERFER_DARK_THEME", () => {
+    const preferDarkTheme = setPreferDarkTheme(true);
+
+    const preferLightTheme = setPreferDarkTheme(false);
+
+    expect(userPreferencesReducer(undefined, preferDarkTheme)).toEqual({
+      preferDarkTheme: true,
+    });
+
+    expect(userPreferencesReducer(undefined, preferLightTheme)).toEqual({
+      preferDarkTheme: false,
+    });
+  });
+});
