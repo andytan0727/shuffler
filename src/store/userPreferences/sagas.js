abbea2535da34b0541746b80438abe79fd7424c5
Andy Tan <andytan0727@gmail.com>
2019-06-28 14:34:40 +0800

    fix: most of the broken ui colors in light mode

--- src/store/userPreferences/sagas.js
+++ src/store/userPreferences/sagas.js
@@ -11,13 +11,70 @@ export function* setPreferDarkThemeWatcher() {
     const action = yield take(SET_PREFER_DARK_THEME);
     const preferDarkTheme = action.payload.isPreferDarkTheme;
 
-    // Set panel color according to theme preference
-    // /playlistInput
-    const panelBgColor = preferDarkTheme
-      ? getRootCssVariable("--panel-dark-bg")
-      : getRootCssVariable("--panel-light-bg");
+    // ===============================
+    // General
+    // ===============================
+    // Set text color
+    setRootCssVariable(
+      "--text-color",
+      preferDarkTheme
+        ? getRootCssVariable("--dark-text")
+        : getRootCssVariable("--light-text")
+    );
 
-    setRootCssVariable("--panel-bg-color", panelBgColor);
+    // Set background color
+    setRootCssVariable(
+      "--bg-color",
+      preferDarkTheme
+        ? getRootCssVariable("--dark-bg")
+        : getRootCssVariable("--light-bg")
+    );
+
+    // Set border color
+    setRootCssVariable(
+      "--border-color",
+      preferDarkTheme
+        ? getRootCssVariable("--dark-border")
+        : getRootCssVariable("--light-border")
+    );
+
+    // Set hover bg color
+    setRootCssVariable(
+      "--hover-bg",
+      preferDarkTheme
+        ? getRootCssVariable("--dark-hover-bg")
+        : getRootCssVariable("--light-hover-bg")
+    );
+
+    // ===============================
+    // /playlistInput route
+    // ===============================
+    // Set panel color according to theme preference
+    setRootCssVariable(
+      "--panel-bg-color",
+      preferDarkTheme
+        ? getRootCssVariable("--panel-dark-bg")
+        : getRootCssVariable("--panel-light-bg")
+    );
+
+    // Set search input color according to theme preference
+    setRootCssVariable(
+      "--search-input-bg-color",
+      preferDarkTheme
+        ? getRootCssVariable("--dark-bg")
+        : getRootCssVariable("--light-bg")
+    );
+
+    // ===============================
+    // /player/ytplayer route
+    // ===============================
+    // Set YT player playlist bg color
+    setRootCssVariable(
+      "--yt-player-list-bg",
+      preferDarkTheme
+        ? getRootCssVariable("--yt-player-list-dark-bg")
+        : getRootCssVariable("--yt-player-list-light-bg")
+    );
   }
 }
 
