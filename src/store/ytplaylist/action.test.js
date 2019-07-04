32b8cb39527e99aaad26e76086fc1664f48c6d20
Andy Tan <andytan0727@gmail.com>
2019-07-04 14:30:51 +0800

    test: add tests for ytplaylist actions and reducer

--- src/store/ytplaylist/action.test.js
+++ src/store/ytplaylist/action.test.js
@@ -5,8 +5,21 @@ import {
   ADD_PLAYING_VIDEOS,
   APPEND_LIST_TO_PLAY,
   REMOVE_PLAYING_PLAYLISTS,
-  REMOVE_PLAYLISTS,
+  DELETE_PLAYLISTS,
   REMOVE_FROM_LIST_TO_PLAY,
+  RENAME_PLAYLIST,
+  SHUFFLE_LIST_TO_PLAY,
+  CLEAR_LIST_TO_PLAY,
+  UPDATE_LIST_TO_PLAY,
+  TOGGLE_PLAYING_VIDEO,
+  REMOVE_VIDEOS_FROM_LIST_TO_PLAY,
+  ADD_VIDEOS_TO_LIST_TO_PLAY,
+  REMOVE_PLAYING_VIDEOS,
+  SET_CHECKED_VIDEOS,
+  DELETE_VIDEOS,
+  REMOVE_PLAYLISTS_FROM_LIST_TO_PLAY,
+  ADD_PLAYLISTS_TO_LIST_TO_PLAY,
+  SET_CHECKED_PLAYLISTS,
 } from "../../utils/constants/actionConstants";
 import {
   addPlaylistAction,
@@ -15,8 +28,21 @@ import {
   addPlayingVideosAction,
   appendListToPlayAction,
   removePlayingPlaylistsAction,
-  removePlaylistsAction,
+  deletePlaylistsAction,
   removeFromListToPlayAction,
+  renamePlaylistAction,
+  shuffleListToPlayAction,
+  clearListToPlayAction,
+  updateListToPlay,
+  togglePlayingVideoAction,
+  removeVideosFromListToPlayAction,
+  addVideosToListToPlayAction,
+  removePlayingVideosAction,
+  setCheckedVideosAction,
+  deleteVideosAction,
+  removePlaylistsFromListToPlayAction,
+  addPlaylistsToListToPlayAction,
+  setCheckedPlaylistsAction,
 } from "./action";
 
 describe("ytplaylist actions", () => {
@@ -30,6 +56,9 @@ describe("ytplaylist actions", () => {
   /** @type {Video} */
   const video = customGlobal.video;
 
+  // =================================
+  // Playlist
+  // =================================
   test("should create ADD_PLAYLIST action object", () => {
     expect(addPlaylistAction(playlist)).toEqual({
       type: ADD_PLAYLIST,
@@ -39,25 +68,46 @@ describe("ytplaylist actions", () => {
     });
   });
 
-  test("should create REMOVE_PLAYLISTS action object", () => {
+  test("should create DELETE_PLAYLISTS action object", () => {
     const playlistIds = [playlist.id];
 
-    expect(removePlaylistsAction(playlistIds)).toEqual({
-      type: REMOVE_PLAYLISTS,
+    expect(deletePlaylistsAction(playlistIds)).toEqual({
+      type: DELETE_PLAYLISTS,
       payload: {
         playlistIds,
       },
     });
   });
 
-  test("should REMOVE_PLAYLISTS payload contains playlistIds [Array]", () => {
+  test("should DELETE_PLAYLISTS payload contains playlistIds [Array]", () => {
     const playlistIds = [playlist.id];
-    const removePlaylistsActionObject = removePlaylistsAction(playlistIds);
+    const removePlaylistsActionObject = deletePlaylistsAction(playlistIds);
 
     // ensure the playlistIds supplied is an array
     expect(Array.isArray(removePlaylistsActionObject.payload.playlistIds)).toBe(
       true
     );
+  });
+
+  test("should create RENAME_PLAYLIST action object", () => {
+    const newName = "newName";
+    const playlistIdToRename = "id-1";
+    expect(renamePlaylistAction(newName, playlistIdToRename)).toEqual({
+      type: RENAME_PLAYLIST,
+      payload: {
+        newName,
+        playlistIdToRename,
+      },
+    });
+  });
+
+  test("should create SET_CHECKED_PLAYLISTS action object", () => {
+    expect(setCheckedPlaylistsAction(["id"])).toEqual({
+      type: SET_CHECKED_PLAYLISTS,
+      payload: {
+        checkedPlaylists: ["id"],
+      },
+    });
   });
 
   test("should create ADD_PLAYING_PLAYLISTS action object", () => {
@@ -92,15 +142,50 @@ describe("ytplaylist actions", () => {
     });
   });
 
-  // ========================
-  // Videos
-  // ========================
+  test("should create ADD_PLAYLISTS_TO_LIST_TO_PLAY action object", () => {
+    expect(addPlaylistsToListToPlayAction(["id"])).toEqual({
+      type: ADD_PLAYLISTS_TO_LIST_TO_PLAY,
+      payload: {
+        playlistIds: ["id"],
+      },
+    });
+  });
 
+  test("should create REMOVE_PLAYLISTS_FROM_LIST_TO_PLAY action object", () => {
+    expect(removePlaylistsFromListToPlayAction(["id"])).toEqual({
+      type: REMOVE_PLAYLISTS_FROM_LIST_TO_PLAY,
+      payload: {
+        playlistIds: ["id"],
+      },
+    });
+  });
+
+  // =================================
+  // Videos
+  // =================================
   test("should create ADD_VIDEO action object", () => {
     expect(addVideoAction(video)).toEqual({
       type: ADD_VIDEO,
       payload: {
         videoToAdd: video,
+      },
+    });
+  });
+
+  test("should create DELETE_VIDEOS action object", () => {
+    expect(deleteVideosAction(["id"])).toEqual({
+      type: DELETE_VIDEOS,
+      payload: {
+        videoIds: ["id"],
+      },
+    });
+  });
+
+  test("should create SET_CHECKED_VIDEOS action object", () => {
+    expect(setCheckedVideosAction(["id"])).toEqual({
+      type: SET_CHECKED_VIDEOS,
+      payload: {
+        checkedVideos: ["id"],
       },
     });
   });
@@ -114,6 +199,45 @@ describe("ytplaylist actions", () => {
     });
   });
 
+  test("should create REMOVE_PLAYING_VIDEOS action object", () => {
+    expect(removePlayingVideosAction(["id"])).toEqual({
+      type: REMOVE_PLAYING_VIDEOS,
+      payload: {
+        videoIds: ["id"],
+      },
+    });
+  });
+
+  test("should create ADD_VIDEOS_TO_LIST_TO_PLAY action", () => {
+    expect(addVideosToListToPlayAction(["id"])).toEqual({
+      type: ADD_VIDEOS_TO_LIST_TO_PLAY,
+      payload: {
+        videoIds: ["id"],
+      },
+    });
+  });
+
+  test("should create REMOVE_VIDEOS_FROM_LIST_TO_PLAY action object", () => {
+    expect(removeVideosFromListToPlayAction(["videoId"])).toEqual({
+      type: REMOVE_VIDEOS_FROM_LIST_TO_PLAY,
+      payload: {
+        videoIds: ["videoId"],
+      },
+    });
+  });
+
+  test("should create TOGGLE_PLAYING_VIDEO action object", () => {
+    expect(togglePlayingVideoAction("videoId")).toEqual({
+      type: TOGGLE_PLAYING_VIDEO,
+      payload: {
+        videoId: "videoId",
+      },
+    });
+  });
+
+  // =================================
+  // List To Play
+  // =================================
   test("should create APPEND_LIST_TO_PLAY action object", () => {
     const items = video.items;
     expect(appendListToPlayAction(items)).toEqual({
@@ -146,6 +270,27 @@ describe("ytplaylist actions", () => {
         itemIds: itemIds.video,
         itemType: "video",
       },
+    });
+  });
+
+  test("should create UPDATE_LIST_TO_PLAY action object", () => {
+    expect(updateListToPlay(playlist.items)).toEqual({
+      type: UPDATE_LIST_TO_PLAY,
+      payload: {
+        listToPlay: playlist.items,
+      },
+    });
+  });
+
+  test("should create CLEAR_LIST_TO_PLAY action object", () => {
+    expect(clearListToPlayAction()).toEqual({
+      type: CLEAR_LIST_TO_PLAY,
+    });
+  });
+
+  test("should create SHUFFLE_LIST_TO_PLAY action object", () => {
+    expect(shuffleListToPlayAction()).toEqual({
+      type: SHUFFLE_LIST_TO_PLAY,
     });
   });
 });
