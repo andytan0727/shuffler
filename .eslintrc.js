7c36a00b215104515cab85d190b35e3b2dc365e9
Andy Tan <andytan0727@gmail.com>
2019-07-18 00:14:21 +0800

    chore: fix eslint issue with hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended",
   ],
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
   env: {
     browser: true,
     jest: true,
@@ -16,10 +16,18 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
+
+    // typescript
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/camelcase": ["error", { properties: "never" }],
     "@typescript-eslint/no-explicit-any": "off",
+
+    // hooks
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+
+    // common
     "no-console": "off",
   },
   settings: {
