08c3b0a10c0720b71c783830d026fa4f3089590a
Andy Tan <andytan0727@gmail.com>
2019-08-01 08:01:49 +0800

    feat: fill each grid item btn with onclick events

--- src/components/Buttons/ManagementPlaylistsPanelGridItemBtn/ManagementPlaylistsPanelGridItemBtn.tsx
+++ src/components/Buttons/ManagementPlaylistsPanelGridItemBtn/ManagementPlaylistsPanelGridItemBtn.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectPlayingPlaylists } from "store/ytplaylist/selector";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 import { Tooltip, IconButton } from "@material-ui/core";
 import {
   PlayArrow as PlayArrowIcon,
@@ -9,6 +9,15 @@ import {
   Add as AddIcon,
   Delete as DeleteIcon,
 } from "@material-ui/icons";
+import { selectNormPlaylistById } from "store/ytplaylist/normSelector";
+import {
+  updateNormListToPlayAction,
+  shuffleNormPlaylistItems,
+  labelNormPlaylistAsPlayingByIdAction,
+  removeNormPlaylistAsPlayingById,
+  deleteNormPlaylistByIdAction,
+} from "store/ytplaylist/normAction";
+import { notify } from "utils/helper/notifyHelper";
 
 interface ManagementPlaylistsPanelGridItemBtnProps {
   playlistId: string;
@@ -21,41 +30,76 @@ interface ManagementPlaylistsPanelGridItemBtnProps {
  *
  */
 const ManagementPlaylistsPanelGridItemBtn = (
-  props: ManagementPlaylistsPanelGridItemBtnProps
+  props: ManagementPlaylistsPanelGridItemBtnProps & RouteComponentProps
 ) => {
-  const { playlistId } = props;
-  const playingPlaylists = useSelector(selectPlayingPlaylists);
+  const { playlistId, history } = props;
+  const dispatch = useDispatch();
+  const playlist = useSelector((state: never) =>
+    selectNormPlaylistById(state, playlistId)
+  );
+  const playlistInPlaying = !!(playlist && playlist.allInPlaying);
+
+  const handlePlayPlaylist = useCallback(() => {
+    // play whole playlist
+    dispatch(
+      updateNormListToPlayAction(
+        "playlists",
+        playlistId,
+        playlist.items as string[]
+      )
+    );
+
+    history.push("/player/ytplayer");
+  }, [dispatch, playlistId, playlist, history]);
+
+  const handleShufflePlaylist = useCallback(() => {
+    dispatch(shuffleNormPlaylistItems(playlistId));
+
+    notify("info", `Shuffling ...`);
+  }, [dispatch, playlistId]);
+
+  const handleAddPlaylistAsPlaying = useCallback(() => {
+    dispatch(labelNormPlaylistAsPlayingByIdAction(playlistId));
+  }, [dispatch, playlistId]);
+
+  const handleRemovePlaylistAsPlaying = useCallback(() => {
+    dispatch(removeNormPlaylistAsPlayingById(playlistId));
+  }, [dispatch, playlistId]);
+
+  const handleDeletePlaylist = useCallback(() => {
+    dispatch(deleteNormPlaylistByIdAction(playlistId));
+  }, [dispatch, playlistId]);
 
   return (
     <div>
       <Tooltip title="Play">
-        <IconButton>
+        <IconButton onClick={handlePlayPlaylist}>
           <PlayArrowIcon fontSize="large" />
         </IconButton>
       </Tooltip>
 
       <Tooltip title="Shuffle">
-        <IconButton>
+        <IconButton onClick={handleShufflePlaylist}>
           <ShuffleIcon fontSize="large" />
         </IconButton>
       </Tooltip>
 
-      {playingPlaylists.includes(playlistId) ? (
+      {playlistInPlaying ? (
         <Tooltip title="Remove Playlist from Now Playing">
-          <IconButton>
+          <IconButton onClick={handleRemovePlaylistAsPlaying}>
             <RemoveIcon fontSize="large" />
           </IconButton>
         </Tooltip>
       ) : (
         <Tooltip title="Add Playlist to Now Playing">
-          <IconButton>
+          <IconButton onClick={handleAddPlaylistAsPlaying}>
             <AddIcon fontSize="large" />
           </IconButton>
         </Tooltip>
       )}
 
       <Tooltip title="Delete">
-        <IconButton>
+        <IconButton onClick={handleDeletePlaylist}>
           <DeleteIcon fontSize="large" />
         </IconButton>
       </Tooltip>
@@ -63,4 +107,4 @@ const ManagementPlaylistsPanelGridItemBtn = (
   );
 };
 
-export default ManagementPlaylistsPanelGridItemBtn;
+export default withRouter(ManagementPlaylistsPanelGridItemBtn);
