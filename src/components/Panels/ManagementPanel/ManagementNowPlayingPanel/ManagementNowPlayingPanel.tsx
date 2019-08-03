416a57d06d486416fed736ea0f53f03b8d95e3a7
Andy Tan <andytan0727@gmail.com>
2019-08-03 10:04:19 +0800

    feat: build up management now playing panel

--- src/components/Panels/ManagementPanel/ManagementNowPlayingPanel/ManagementNowPlayingPanel.tsx
+++ src/components/Panels/ManagementPanel/ManagementNowPlayingPanel/ManagementNowPlayingPanel.tsx
@@ -1,17 +1,66 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
+import { ManagementPanelCtrlBtnGroup } from "components/Buttons";
+import { useCheckbox } from "components/Checkbox/hooks";
+import {
+  createItemData,
+  ListToPlayListItemSecondaryAction,
+  ManagementPanelVirtualList,
+  withListItemSecondaryAction,
+} from "components/Lists/ManagementPanelVirtualList";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RouteComponentProps, withRouter } from "react-router";
+import { deleteNormListToPlayItemsAction } from "store/ytplaylist/normAction";
+import { selectAllNormListToPlayItemIds } from "store/ytplaylist/normSelector";
+import { shuffleListToPlayAction } from "store/ytplaylist/sharedAction";
+
+import { Divider, Typography } from "@material-ui/core";
 
 import styles from "./styles.module.scss";
 
-const ManagementNowPlayingPanel = () => {
+type ManagementNowPlayingPanelProps = RouteComponentProps;
+
+const ManagementPanelVirtualListToPlayItem = withListItemSecondaryAction(
+  ListToPlayListItemSecondaryAction
+);
+
+const ManagementNowPlayingPanel = (props: ManagementNowPlayingPanelProps) => {
+  const { history } = props;
+  const dispatch = useDispatch();
+  const listToPlayItemIds = useSelector(selectAllNormListToPlayItemIds);
+  const { checked, handleSetChecked } = useCheckbox();
+  const listToPlayItemData = createItemData(
+    checked,
+    handleSetChecked,
+    listToPlayItemIds
+  );
+
+  const handlePlayListToPlay = useCallback(() => {
+    history.push("/player/ytplayer");
+  }, [history]);
+
+  const handleShuffleListToPlay = useCallback(() => {
+    dispatch(shuffleListToPlayAction());
+  }, [dispatch]);
+
+  // delete listToPlay items without deleting original items from playlists/videos
+  const handleDeleteListToPlayItems = useCallback(() => {
+    dispatch(deleteNormListToPlayItemsAction(checked));
+  }, [dispatch, checked]);
+
   return (
     <div className={styles.managementNowPlayingPanelDiv}>
       <Typography variant="h4">Now Playing</Typography>
-      <div className={styles.underConstruction}>
-        <Typography variant="h2">Under Construction</Typography>
-      </div>
+      <ManagementPanelCtrlBtnGroup
+        handlePlay={handlePlayListToPlay}
+        handleShuffle={handleShuffleListToPlay}
+        handleDelete={handleDeleteListToPlayItems}
+      />
+      <Divider />
+      <ManagementPanelVirtualList itemData={listToPlayItemData}>
+        {ManagementPanelVirtualListToPlayItem}
+      </ManagementPanelVirtualList>
     </div>
   );
 };
 
-export default ManagementNowPlayingPanel;
+export default withRouter(ManagementNowPlayingPanel);
