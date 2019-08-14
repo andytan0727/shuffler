0047dd649ab4bbb4f60514aa296168784a48da4c
Andy Tan <andytan0727@gmail.com>
2019-08-14 23:36:01 +0800

    feat: create FilteredSnippetInput component

---
+++ src/components/Inputs/FilterSnippetInput/FilterSnippetInput.tsx
@@ -0,0 +1,61 @@
+import debounce from "lodash/debounce";
+import React, { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "store";
+import {
+  clearFilteredSnippets,
+  createFuse,
+  fuzzySearchSnippetsByTitle,
+} from "store/ytplaylist/normAction";
+import { selectNormSnippetsByItemIds } from "store/ytplaylist/normSelector";
+
+import TextField from "@material-ui/core/TextField";
+
+export interface FilterSnippetInputProps {
+  itemIds: string[];
+}
+
+const FilterSnippetInput = (props: FilterSnippetInputProps) => {
+  const { itemIds } = props;
+  const snippets = useSelector((state: AppState) =>
+    selectNormSnippetsByItemIds(state, itemIds)
+  );
+  const dispatch = useDispatch();
+
+  const debouncedSearch = debounce((value: string) => {
+    dispatch(fuzzySearchSnippetsByTitle(value));
+  }, 50);
+
+  const handleSearch = useCallback(
+    (e: InputChangeEvent) => {
+      const value = e.target.value;
+
+      if (!value) {
+        dispatch(clearFilteredSnippets());
+      } else {
+        debouncedSearch(value);
+      }
+    },
+    [debouncedSearch, dispatch]
+  );
+
+  // create fuse on mount and on snippets change
+  useEffect(() => {
+    dispatch(createFuse(snippets));
+  }, [dispatch, snippets]);
+
+  return (
+    <React.Fragment>
+      <TextField
+        id="filter"
+        label="filter"
+        placeholder="filter by title"
+        onChange={handleSearch}
+        margin="dense"
+        variant="outlined"
+      />
+    </React.Fragment>
+  );
+};
+
+export default FilterSnippetInput;
--- src/components/Inputs/index.ts
+++ src/components/Inputs/index.ts
@@ -2,3 +2,6 @@ export { useSearchYT } from "./hooks";
 export { makeSearchInput } from "./SearchInput";
 export { makeManagementPanelSearchInput } from "./ManagementPanelSearchInput";
 export { default as RenameInput } from "./RenameInput";
+export {
+  default as FilterSnippetInput,
+} from "./FilterSnippetInput/FilterSnippetInput";
