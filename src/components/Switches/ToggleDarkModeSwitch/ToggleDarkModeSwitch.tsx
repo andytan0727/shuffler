aabba1732d575afd86773a828b44d7afea08dc29
Andy Tan <andytan0727@gmail.com>
2019-08-29 07:51:00 +0800

    fix: mini-css-extract-plugin css conflicting order

--- src/components/Switches/ToggleDarkModeSwitch/ToggleDarkModeSwitch.tsx
+++ src/components/Switches/ToggleDarkModeSwitch/ToggleDarkModeSwitch.tsx
@@ -3,9 +3,35 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPreferDarkTheme } from "store/userPreferences/action";
 import { selectPreferDarkTheme } from "store/userPreferences/selector";
 
-import styles from "./styles.module.scss";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  toggleDarkModeSwitch: {
+    height: "1px",
+    width: "1px",
+    overflow: "hidden",
+  },
+  toggleDarkModeSwitchLabel: {
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
+    right: "2%",
+    cursor: "pointer",
+
+    "&::before": {
+      content: "var(--switch-icon)",
+      fontSize: "40px",
+      transition: "text-shadow 0.2s",
+    },
+
+    "&:hover::before": {
+      textShadow: "0 0 15px var(--switch-shadow-color)",
+    },
+  },
+});
 
 const ToggleDarkModeSwitch = () => {
+  const classes = useStyles();
   const preferDarkTheme = useSelector(selectPreferDarkTheme);
   const dispatch = useDispatch();
   const [checked, setChecked] = useState(preferDarkTheme);
@@ -24,10 +50,10 @@ const ToggleDarkModeSwitch = () => {
 
   return (
     <div>
-      <label className={styles.toggleDarkModeSwitchLabel}>
+      <label className={classes.toggleDarkModeSwitchLabel}>
         <input
           type="checkbox"
-          className={styles.toggleDarkModeSwitch}
+          className={classes.toggleDarkModeSwitch}
           checked={checked}
           onChange={handleToggleDarkMode}
         />
