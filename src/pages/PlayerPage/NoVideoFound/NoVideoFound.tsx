36eca8506ce9f2a1ed5bd66f32875dfe4f2b0b92
Andy Tan <andytan0727@gmail.com>
2019-10-23 08:43:49 +0800

    feat: add SearchPlaylistBtn to NoVideoFound

--- src/pages/PlayerPage/NoVideoFound/NoVideoFound.tsx
+++ src/pages/PlayerPage/NoVideoFound/NoVideoFound.tsx
@@ -1,16 +1,21 @@
 import novideo from "assets/novideo.svg";
+import { SearchPlaylistBtn } from "components/Buttons";
 import React from "react";
 
 import styles from "./styles.module.scss";
 
 const NoVideoFound = () => (
   <div className={styles.noVideo}>
-    <h2>
-      <span role="img" aria-label="no-video-found">
-        ⚠️
-      </span>
-      No Video Found
-    </h2>
+    <div className={styles.noVideoTitleDiv}>
+      <h2>
+        <span role="img" aria-label="no-video-found">
+          ⚠️
+        </span>
+        No Video Found. Add some playlist?
+      </h2>
+      <SearchPlaylistBtn />
+    </div>
+
     <img src={novideo} alt="no video" />
   </div>
 );
