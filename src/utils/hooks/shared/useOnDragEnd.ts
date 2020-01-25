68dfc5cac8329399740ee2466100a7cda39fd3e9
Andy Tan <andytan0727@gmail.com>
2020-01-25 19:56:16 +0800

    refactor: remove drag hooks code duplication

---
+++ src/utils/hooks/shared/useOnDragEnd.ts
@@ -0,0 +1,64 @@
+import { useCallback } from "react";
+import { DropResult } from "react-beautiful-dnd";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurSongIdx, selectPlaying } from "store/ytplayer/selector";
+import { reorderListToPlayItemAction } from "store/ytplaylist/listToPlayActions";
+import { reorderPlaylistItemByPlaylistIdAction } from "store/ytplaylist/playlistActions";
+import { reorderVideoItem } from "store/ytplaylist/videoActions";
+import { ActionType } from "typesafe-actions";
+import { isFiltering } from "utils/helper/generalHelper";
+import { notify } from "utils/helper/notifyHelper";
+
+export const useOnDragEnd = (
+  curItemCount: number,
+  reorderAction: (
+    fromIdx: number,
+    toIdx: number
+  ) => ActionType<
+    | typeof reorderListToPlayItemAction
+    | typeof reorderPlaylistItemByPlaylistIdAction
+    | typeof reorderVideoItem
+  >
+) => {
+  const isPlayerPlaying = useSelector(selectPlaying);
+  const curSongIdx = useSelector(selectCurSongIdx);
+  const dispatch = useDispatch();
+
+  console.log(curItemCount);
+
+  const handleOnDragEnd = useCallback(
+    (oriItemCount: number) => (result: DropResult) => {
+      if (!result.destination) {
+        return;
+      }
+
+      const fromIdx = result.source.index;
+      const toIdx = result.destination.index;
+
+      if (fromIdx === toIdx) {
+        return;
+      }
+
+      // NOTE: experimental.
+      // Test whether this condition is suitable or not
+      // to prohibit the drag action
+      if (fromIdx === curSongIdx && isPlayerPlaying) {
+        notify("warning", "Please don't move the first item while playing");
+        return;
+      }
+
+      // disable drag n drop on filtering
+      if (isFiltering(oriItemCount, curItemCount)) {
+        notify("warning", "Please don't drag while filtering");
+        return;
+      }
+
+      dispatch(reorderAction(fromIdx, toIdx));
+    },
+    [curItemCount, curSongIdx, dispatch, isPlayerPlaying, reorderAction]
+  );
+
+  return {
+    handleOnDragEnd,
+  };
+};
--- src/utils/hooks/videosHooks/useDragVideoItem.ts
+++ src/utils/hooks/videosHooks/useDragVideoItem.ts
@@ -1,50 +1,12 @@
-import { useCallback } from "react";
-import { DropResult } from "react-beautiful-dnd";
-import { useDispatch, useSelector } from "react-redux";
-import { selectCurSongIdx, selectPlaying } from "store/ytplayer/selector";
+import { useSelector } from "react-redux";
 import { reorderVideoItem } from "store/ytplaylist/videoActions";
 import { selectVideoItemCount } from "store/ytplaylist/videoSelectors";
-import { isFiltering } from "utils/helper/generalHelper";
-import { notify } from "utils/helper/notifyHelper";
+
+import { useOnDragEnd } from "../shared/useOnDragEnd";
 
 export const useDragVideoItem = () => {
-  const isPlayerPlaying = useSelector(selectPlaying);
-  const curSongIdx = useSelector(selectCurSongIdx);
   const videoItemCount = useSelector(selectVideoItemCount);
-  const dispatch = useDispatch();
-
-  const handleOnDragEnd = useCallback(
-    (itemCount: number) => (result: DropResult) => {
-      if (!result.destination) {
-        return;
-      }
-
-      const fromIdx = result.source.index;
-      const toIdx = result.destination.index;
-
-      if (fromIdx === toIdx) {
-        return;
-      }
-
-      // NOTE: experimental.
-      // Test whether this condition is suitable or not
-      // to prohibit the drag action
-      if (fromIdx === curSongIdx && isPlayerPlaying) {
-        notify("warning", "Please don't move the first item while playing");
-        return;
-      }
-
-      // just put as consistency since video panel
-      // doesn't have filter function
-      if (isFiltering(itemCount, videoItemCount)) {
-        notify("warning", "Please don't drag while filtering");
-        return;
-      }
-
-      dispatch(reorderVideoItem(fromIdx, toIdx));
-    },
-    [curSongIdx, dispatch, isPlayerPlaying, videoItemCount]
-  );
+  const { handleOnDragEnd } = useOnDragEnd(videoItemCount, reorderVideoItem);
 
   return {
     handleOnDragEnd,
