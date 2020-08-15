41400448340d95020d5535c177ea2a752388058c
Andy Tan <andytan0727@gmail.com>
2020-08-15 20:24:50 +0800

    feat: resizable player

--- src/pages/PlayerPage/YTPlayerPage/YTPlayerPage.tsx
+++ src/pages/PlayerPage/YTPlayerPage/YTPlayerPage.tsx
@@ -4,9 +4,12 @@ import { PlayerList } from "components/Lists";
 import VideoPlayer from "components/Players/Video/VideoPlayer";
 import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Split from "react-split";
 import { setCurSongIdx } from "store/ytplayer/action";
 import { selectCurSongIdx } from "store/ytplayer/selector";
 import { selectListToPlayResultSnippets } from "store/ytplaylist/listToPlaySelectors";
+
+import { useTheme } from "@material-ui/core/styles";
 
 import NoVideoFound from "../NoVideoFound";
 import styles from "./styles.module.scss";
@@ -16,6 +19,7 @@ const YTPlayerPage = () => {
   const listToPlaySnippets = useSelector(selectListToPlayResultSnippets);
   const dispatch = useDispatch();
   const currentSnippet = listToPlaySnippets[curSongIdx];
+  const theme = useTheme();
 
   const [open, setOpen] = useState(false);
 
@@ -39,7 +43,24 @@ const YTPlayerPage = () => {
   }, []);
 
   return listToPlaySnippets.length !== 0 ? (
-    <div className={styles.ytPlayerDiv}>
+    <Split
+      className={styles.ytPlayerDiv}
+      sizes={[75, 25]}
+      minSize={[320, 400]}
+      expandToMin={false}
+      gutterSize={10}
+      gutterAlign="center"
+      snapOffset={30}
+      dragInterval={1}
+      direction="horizontal"
+      cursor="col-resize"
+      gutterStyle={() => ({
+        width: "6px",
+        height: "15%",
+        "background-color": theme.palette.secondary.main,
+        "align-self": "center",
+      })}
+    >
       <div className={styles.player}>
         <h3 className={styles.videoTitle}>
           {currentSnippet && currentSnippet.title}
@@ -69,7 +90,7 @@ const YTPlayerPage = () => {
         {/* a temporary dialog with portable playlistInput LgPanel */}
         <LgPanelDialog open={open} handleCloseDialog={handleCloseDialog} />
       </div>
-    </div>
+    </Split>
   ) : (
     <NoVideoFound />
   );
---
+++ src/typings/react-split.d.ts
@@ -0,0 +1,65 @@
+// remove this if react-split types are officially supported
+declare module "react-split" {
+  export interface SplitProps
+    extends import("react").HTMLProps<HTMLDivElement> {
+    // Initial sizes of each element in percents or CSS values.
+    sizes?: number[];
+
+    // Minimum size of each element.
+    minSize?: number | number[];
+
+    expandToMin?: boolean;
+
+    // Gutter size in pixels.
+    gutterSize?: number;
+
+    gutterAlign?: string;
+
+    // Snap to minimum size offset in pixels.
+    snapOffset?: number;
+
+    dragInterval?: number;
+
+    // Direction to split: horizontal or vertical.
+    direction?: "horizontal" | "vertical";
+
+    // Cursor to display while dragging.
+    cursor?: string;
+
+    // Callback on drag.
+    onDrag?(): void;
+
+    // Callback on drag start.
+    onDragStart?(): void;
+
+    // Callback on drag end.
+    onDragEnd?(): void;
+
+    // Called to create each gutter element
+    gutter?(index: number, direction: "horizontal" | "vertical"): HTMLElement;
+
+    // Called to set the style of each element.
+    elementStyle?(
+      dimension: "width" | "height",
+      elementSize: number,
+      gutterSize: number,
+      index: number
+    ): CSSStyleDeclarationPartial;
+
+    // Called to set the style of the gutter.
+    gutterStyle?(
+      dimension: "width" | "height",
+      gutterSize: number,
+      index: number
+    ): CSSStyleDeclarationPartial;
+
+    className?: string;
+    style?: import("react").CSSProperties;
+
+    children: import("react").ReactNode;
+  }
+
+  const Split: (props: SplitProps) => import("react").ReactElement;
+
+  export default Split;
+}
